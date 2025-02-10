a228bed765fbf04dabcfd377c47a9530bcdd399f
QuiltyVal <68616138+QuiltyVal@users.noreply.github.com>
2025-02-10 15:59:28 +0100

    поменял путь в индексе на мейн

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Импорт маршрутов
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App"; // Основное приложение
 import Login from "./Login"; // Страница логина
 
@@ -12,6 +12,8 @@ ReactDOM.render(
         <Route path="/" element={<App />} />
         {/* Страница логина */}
         <Route path="/login" element={<Login />} />
+        {/* Новый маршрут /main, который тоже рендерит основное приложение */}
+        <Route path="/main" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>,
