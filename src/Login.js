a7bd9e93df774a688c22d590285a3fe6b945c01f
QuiltyVal <68616138+QuiltyVal@users.noreply.github.com>
2025-02-11 10:32:34 +0100

    добавил консоль лог в логин жс

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,23 @@
+import { useEffect } from "react";
+
 export default function Login() {
+    useEffect(() => {
+        console.log("URL:", window.location.search);
+        console.log("LocalStorage:", localStorage.getItem("telegramUser"));
+    }, []); // Пустой массив зависимостей означает, что код выполнится только при монтировании компонента
+
     return (
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Войдите через Telegram</h1>
-        <script
-          async
-          src="https://telegram.org/js/telegram-widget.js?7"
-          data-telegram-login="Fegefeuerbot"
-          data-size="large"
-          data-radius="5"
-          data-auth-url="https://dulcet-yeot-cb2d95.netlify.app/"
-          data-request-access="write"
-        ></script>
-      </div>
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Войдите через Telegram</h1>
+            <script
+                async
+                src="https://telegram.org/js/telegram-widget.js?7"
+                data-telegram-login="Fegefeuerbot"
+                data-size="large"
+                data-radius="5"
+                data-auth-url="https://dulcet-yeot-cb2d95.netlify.app/"
+                data-request-access="write"
+            ></script>
+        </div>
     );
-  }
-  
+}
